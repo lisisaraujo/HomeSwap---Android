139286dd08bf3d21c3c524ffe1e07be9b34ded5e
Lisis Ruschel <laruschel@proton.me>
2024-06-18 11:55:20 +0200

    added tab menu in homepage to switch between userslist and apartmentslist

---
+++ app/src/main/java/com/example/homeswap_android/ui/home/HomeFragment.kt
@@ -0,0 +1,51 @@
+package com.example.homeswap_android.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.example.homeswap_android.adapter.ViewPagerAdapter
+import com.example.homeswap_android.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayout
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragments = listOf(UsersListHomeFragment(), ApartmentsListHomeFragment())
+        val adapter = ViewPagerAdapter(requireActivity().supportFragmentManager, lifecycle, fragments)
+
+        binding.viewPager.adapter = adapter
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Users"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Apartments"))
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                binding.viewPager.currentItem = tab?.position ?: 0
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                binding.tabLayout.selectTab(binding.tabLayout.getTabAt(position))
+            }
+        })
+    }
+}
